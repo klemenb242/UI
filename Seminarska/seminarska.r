2d10cf9c30e372035d14c1cae85cfe654bb48699
irtgal <gal.irt.01@gmail.com>
2022-11-21 17:09:22 +0100

    Update seminarska.r

--- Seminarska/seminarska.r
+++ Seminarska/seminarska.r
@@ -3,37 +3,18 @@
 
 setwd("C:/Users/Gal/Documents/FRI/UI/Seminarska/")
 md <- read.table(file="nbadata.txt", sep=",", header=TRUE)
-# anotatiion
-# print(md$gmDate)
-# print(md$homeAbbr)
-# print(md$homeDayOff)
-# print(md$homePTS)
-# print(md$homeAST)
-# print(md$homeTO)
-# print(md$homeSTL)
-# print(md$homeBLK)
-# print(md$homePF)
-# print(md$homeFGA)
-# print(md$homeFGM)
-# print(md$homeDayOff)
-# print(md$awayDayOff)
-# print(md$awayFGM);
 
-# nekaj uporabnih funkcij
-summary(md)
-names(md)
-
-h <- vector()
 
 SEASONS = unique(md$gmSeason);
 TEAMS = unique(c(md$awayAbbr, md$homeAbbr))
 
 # povprecno stevilo metov trojk obeh ekip skupaj na sezono
+averageThrees <- vector()
 for(season in SEASONS) {
     seasonGames <- md$gmSeason == season;
-    h <- c(h, mean(md$home3PA[seasonGames] + md$away3PA[seasonGames]));
+    averageThrees <- c(averageThrees, mean(md$home3PA[seasonGames] + md$away3PA[seasonGames]));
 }
-barplot(h, names=SEASONS, main="Povprecno število vseh metov za tri pike v tekmi na sezono");
+barplot(averageThrees, names=SEASONS, main="Povprecno število vseh metov za tri pike v tekmi na sezono");
 
 # stevilo zmag home ekip vs stevilo zmag away ekip
 homeWins = table(md$homePTS > md$awayPTS);
@@ -70,11 +51,9 @@ barplot(winRatioByTeam[1:5], names=names(winRatioByTeam)[1:5], main="Top 5 najbo
 
 homeFreeShotSuccessRatio = md$homeFTM / md$homeFTA;
 awayFreeShotSuccessRatio = md$awayFTM / md$awayFTA;
-totalFreeShotSuccessRatio = append(homeShotSuccessRatio, awayShotSuccessRatio);
-
+totalFreeShotSuccessRatio = append(homeFreeShotSuccessRatio, awayFreeShotSuccessRatio);
 daysOff = append(md$homeDayOff, md$awayDayOff);
-
-plot(totalFreeShotSuccessRatio,daysOff, main="Razmerje uspešnih metov za tri pike domačih ekip odvisno od števila prostih dni", xlab="Število prostih dni", ylab="Razmerje uspešnih metov za tri pike");
+plot(totalFreeShotSuccessRatio,daysOff, main="Razmerje uspešnih prostih metov odvisno od števila prostih dni", xlab="Število prostih dni", ylab="Razmerje uspešnih metov za tri pike");
 
 
 
